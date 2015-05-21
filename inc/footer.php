8ddc7079b26cd2f04be9d6398fc45722bc2ebbe7
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-05-20 22:25:56 -0400

    Add php to display date in footer.

--- inc/footer.php
+++ inc/footer.php
@@ -5,7 +5,7 @@
         <a href="https://github.com/kmcmurtrey"><img src="img/Github.png" alt="Github Logo" class="social-icon"></a>
         <a href="https://www.linkedin.com/in/koreymcmurtrey/"><img src="img/LinkedIN.png" alt="LinkedIn Logo" class="social-icon"></a>
         <a href="http://twitter.com/koreymc"><img src="img/twitter.png" alt="Twitter Logo" class="social-icon"></a>
-        <p>&copy; 2015 Korey McMurtrey.</p>
+        <p>&copy;<?php echo date("Y"); ?> Korey McMurtrey.</p>
     </footer>
 </div>
 </body>

d9ce05c6b3105e5731a16fe16dc564e097808183
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-11-02 21:22:56 -0500

    Updates About page content

--- about.php
+++ about.php
@@ -9,7 +9,7 @@ include("inc/header.php");
         
         <h3>About</h3>
         
-        <p>Hi, I'm Korey McMurtrey.  I just completed the front-end web development course and am currently studying PHP at <a href="http://www.codelouisville.org/">Code Louisville</a>.</p>
+        <p>Hi, I'm Korey McMurtrey.  I have completed the front-end web development course and the PHP course at <a href="http://www.codelouisville.org/">Code Louisville</a>.</p>
         <p>My goal is to become a full-time web developer. Though coding and learning take up most of my free time these days, it rarely feels like work. I love solving coding problems and am always looking for something new to learn or work on.</p>
         <p>When I'm not developing and studying, I enjoy game nights with friends, catching up on my favorite shows, and a good cup of coffee.</p>
         

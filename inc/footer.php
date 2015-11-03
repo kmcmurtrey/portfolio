bc7941b872f22e413b4619b9c88bf74ca0b8aef7
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-11-02 21:07:06 -0500

    Removed twitter link from footer

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,6 @@
     <footer>
         <a href="https://github.com/kmcmurtrey"><img src="img/Github.png" alt="Github Logo" class="social-icon"></a>
         <a href="https://www.linkedin.com/in/koreymcmurtrey/"><img src="img/LinkedIN.png" alt="LinkedIn Logo" class="social-icon"></a>
-        <a href="http://twitter.com/koreymc"><img src="img/twitter.png" alt="Twitter Logo" class="social-icon"></a>
         <p>&copy;<?php echo date("Y"); ?> Korey McMurtrey.</p>
     </footer>
 </div>

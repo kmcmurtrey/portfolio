53d56d92b84529e6ce0dba6c0b9e234178c0eaa2
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-11-02 21:06:19 -0500

    Adjusts li sizes on gallery

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include("inc/header.php");
 ?>
 
       <section class="headline3">
-        <h3>Projects</h3>
+        <h3>Projects and Practice</h3>
         <ul id="gallery">
           <li>
             <a href="https://github.com/kmcmurtrey/web_dev_resources">
@@ -35,19 +35,16 @@ include("inc/header.php");
             <a href="https://github.com/CodeLouisville/wordgame">
               <img src="img/Github.png" alt="Github Logo" class="social-icon">
             </a>
-          </li>
-            </ul>
-
-            <h3>Practice</h3>
-            <li>
-            <p data-height="350" data-theme-id="14595" data-slug-hash="yyQGVj" data-default-tab="result" data-user="kmcmurtrey" class='codepen'>See the Pen <a href='http://codepen.io/kmcmurtrey/pen/yyQGVj/'>JavaScript Practice 7 - Bubble Sort</a> by Korey McMurtrey (<a href='http://codepen.io/kmcmurtrey'>@kmcmurtrey</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
+          </li><!--
+          --><li>
+            <p data-height="380" data-theme-id="14595" data-slug-hash="yyQGVj" data-default-tab="result" data-user="kmcmurtrey" class='codepen'>See the Pen <a href='http://codepen.io/kmcmurtrey/pen/yyQGVj/'>JavaScript Practice 7 - Bubble Sort</a> by Korey McMurtrey (<a href='http://codepen.io/kmcmurtrey'>@kmcmurtrey</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
             <script async src="http://assets.codepen.io/assets/embed/ei.js"></script>
           </li><!--
           --><li>
-            <p data-height="350" data-theme-id="14595" data-slug-hash="ogJqzq" data-default-tab="result" data-user="kmcmurtrey" class='codepen'>See the Pen <a href='http://codepen.io/kmcmurtrey/pen/ogJqzq/'>JavaScript Practice 9 - Moving Shape</a> by Korey McMurtrey (<a href='http://codepen.io/kmcmurtrey'>@kmcmurtrey</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
+            <p data-height="380" data-theme-id="14595" data-slug-hash="ogJqzq" data-default-tab="result" data-user="kmcmurtrey" class='codepen'>See the Pen <a href='http://codepen.io/kmcmurtrey/pen/ogJqzq/'>JavaScript Practice 9 - Moving Shape</a> by Korey McMurtrey (<a href='http://codepen.io/kmcmurtrey'>@kmcmurtrey</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
             <script async src="//assets.codepen.io/assets/embed/ei.js"></script>
           </li>
-
+        </ul>
         
       </section>
 
